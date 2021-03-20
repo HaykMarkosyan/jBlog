93516bdd5ae682c91274bd2569471bff47aa9a29
HaykMarkosyan <hayk.markosyan.10@gmail.com>
2021-03-20 20:14:03 +0400

    jblogforu

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get("/new", function (req, res) {
 
 app.get("/", function (req, res) {
     if (logined == true) {
-			passport.authenticate("local")(req, res, function () {	
+			passport.authenticate("local-login")(req, res, function () {	
         Posts.find({}, function (err, foundItems) {
             if (!err) {
                 res.render("home", { posts: foundItems })
