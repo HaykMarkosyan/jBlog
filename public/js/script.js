a234d872e5a7e64fa00a8590bfbc0a1da3ec30d0
root <root@DESKTOP-7F3JCSI.localdomain>
2021-04-11 01:58:13 +0400

    jBlog

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
-likeCheckedBox = document.getElementById("btn-check-outlined");
-likedcount = document.getElementById("likec");
-function checklike(){
-	if (likeCheckedBox.checked == true){
-		likedcount.innerHTML++;
-	} else if(likeCheckedBox.checked == false){
-		likedcount.innerHTML--;
-	}
-}
+// likeCheckedBox = document.getElementById("btn-check-outlined");
+// likedcount = document.getElementById("likecount");
+// function checklike(){
+// 	if (likeCheckedBox.checked == true){
+// 		likedcount.innerHTML++;
+// 	} else if(likeCheckedBox.checked == false){
+// 		likedcount.innerHTML--;
+// 	}
+// }
