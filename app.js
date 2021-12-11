e2f7631fc9e920ea68a4f9307af4a95b7a2a73ff
Hayk <77397812+HaykMarkosyan@users.noreply.github.com>
2021-12-11 06:05:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-mongoose.connect("mongodb+srv://admin-hayk:Test@cluster0.d8y9j.mongodb.net/jBlogDB", { useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-hayk:<password>@cluster0.d8y9j.mongodb.net/jBlogDB", { useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
